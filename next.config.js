6a7da2ddd69a2608ff637f457ef5e2b5b168e654
Alec Leong <leongalec@gmail.com>
2020-08-17 02:29:20 -0700

    Next.js configuration.

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  /** Add environment variables to the JavaScript bundle.
+   * Source: https://nextjs.org/docs/api-reference/next.config.js/environment-variables
+   */
+  env: {
+    BITCOIN_PRICE_INDEX_URL: 'http://52.53.133.44:3000/',
+    TRAVELSITE_REVIEWS_SERVER_URL: 'http://204.236.151.209:3001/',
+  },
+}
