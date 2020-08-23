2942cc136e083fd98231341c4451a516f5090184
Alec Leong <leongalec@gmail.com>
2020-08-22 17:19:04 -0700

    Updated <BitcoinPriceIndex /> to redirect page instead of <iframe />.

--- pages/bitcoin-price-index/index.js
+++ pages/bitcoin-price-index/index.js
@@ -1,15 +1,29 @@
-export default function BitCoinPriceIndex() {
-  return (
-    <iframe src={process.env.BITCOIN_PRICE_INDEX_URL || ''}>
-      <style jsx>{`
-        iframe {
-          background-color: #FFFFFF;
-          border: none;
-          height: 100vmax;
-          overflow: hidden;
-          width: 100%;
-        }
-        `}</style>
-    </iframe>
-  )
-};
+class BitCoinPriceIndex extends React.Component {
+  componentDidMount() {
+    window.open(process.env.BITCOIN_PRICE_INDEX_URL, '_blank');
+  }
+
+  render() {
+    return (
+      <p>Redirecting to <a href={process.env.BITCOIN_PRICE_INDEX_URL}>{process.env.BITCOIN_PRICE_INDEX_URL}</a></p>
+    )
+  }
+}
+
+export default BitCoinPriceIndex;
+
+// export default function BitCoinPriceIndex() {
+//   return (
+//     <iframe src={process.env.BITCOIN_PRICE_INDEX_URL || ''}>
+//       <style jsx>{`
+//         iframe {
+//           background-color: #FFFFFF;
+//           border: none;
+//           height: 100vmax;
+//           overflow: hidden;
+//           width: 100%;
+//         }
+//         `}</style>
+//     </iframe>
+//   )
+// };
