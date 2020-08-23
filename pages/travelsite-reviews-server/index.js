22b32641e3e282122f5e40e08ad122379aceb201
Alec Leong <leongalec@gmail.com>
2020-08-22 17:20:28 -0700

    Updated <TravelsiteReviewsServer /> to redirect page instead of <iframe />.

--- pages/travelsite-reviews-server/index.js
+++ pages/travelsite-reviews-server/index.js
@@ -1,14 +1,28 @@
-export default function TravelSiteReviewsServer() {
-  return (
-    <iframe src={process.env.TRAVELSITE_REVIEWS_SERVER_URL || ''}>
-      <style jsx>{`
-        iframe {
-          border: none;
-          height: 100vmax;
-          overflow: hidden;
-          width: 100%;
-        }
-        `}</style>
-    </iframe>
-  )
-};
+class TravelSiteReviewsServer extends React.Component {
+  componentDidMount() {
+    window.open(process.env.TRAVELSITE_REVIEWS_SERVER_URL, '_blank');
+  }
+
+  render() {
+    return (
+      <p>Redirecting to <a href={process.env.TRAVELSITE_REVIEWS_SERVER_URL}>{process.env.TRAVELSITE_REVIEWS_SERVER_URL}</a></p>
+    )
+  }
+}
+
+export default TravelSiteReviewsServer;
+
+// export default function TravelSiteReviewsServer() {
+//   return (
+//     <iframe src={process.env.TRAVELSITE_REVIEWS_SERVER_URL || ''}>
+//       <style jsx>{`
+//         iframe {
+//           border: none;
+//           height: 100vmax;
+//           overflow: hidden;
+//           width: 100%;
+//         }
+//         `}</style>
+//     </iframe>
+//   )
+// };
