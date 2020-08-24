5f4612e9d5c98de0ac69f74e0601ffd9fdc2eda7
Alec Leong <leongalec@gmail.com>
2020-08-23 18:42:59 -0700

    Updated env variable BITCOIN_PRICE_INDEX_URL value.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
    * Source: https://nextjs.org/docs/api-reference/next.config.js/environment-variables
    */
   env: {
-    BITCOIN_PRICE_INDEX_URL: 'http://52.53.133.44:3000/',
+    BITCOIN_PRICE_INDEX_URL: 'http://50.18.35.181:3000/',
     TRAVELSITE_REVIEWS_SERVER_URL: 'http://204.236.151.209:3001/',
   },
 }
